f11541f770bf89361046c96675a1c215fc539f4c
Stephen McMullan <stephen.mcmullan@netscout.com>
2022-08-05 08:31:18 +0100

    Added metrics and custom source

--- src/main/scala/RateSourceSparkStreamTest.scala
+++ src/main/scala/RateSourceSparkStreamTest.scala
@@ -1,8 +1,11 @@
-import org.apache.spark.metrics.source.LongAccumulatorSource
+import com.codahale.metrics.{Gauge, Metric, MetricRegistry, MetricSet}
+import org.apache.spark.metrics.source.StatsSource
 import org.apache.spark.sql.{ForeachWriter, SparkSession}
 import org.apache.spark.util.LongAccumulator
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, SparkContext, SparkEnv}
 import org.slf4j.LoggerFactory
+
+import java.util
 
 object RateSourceSparkStreamTest {
   def main(args: Array[String]): Unit = {
@@ -15,7 +18,14 @@ object RateSourceSparkStreamTest {
       .getOrCreate()
 
     val stats = new PartitionStats(spark.sparkContext)
-    LongAccumulatorSource.register(spark.sparkContext, stats.statsMap)
+    val statsMap = stats.getStatsMap
+
+    val metricRegistry = new MetricRegistry
+    metricRegistry.registerAll(stats)
+
+    SparkEnv.get.metricsSystem.registerSource(
+      StatsSource("RateSourceSparkStreamTest", metricRegistry)
+    )
 
     import spark.implicits._
 
@@ -27,7 +37,7 @@ object RateSourceSparkStreamTest {
       .load()
       .map(row => row.mkString("##"))
       .writeStream
-      .foreach(new ForeachWriterImpl(stats))
+      .foreach(new ForeachWriterImpl(statsMap))
       .start()
 
     spark.streams.awaitAnyTermination()
@@ -36,42 +46,76 @@ object RateSourceSparkStreamTest {
   }
 }
 
-class ForeachWriterImpl(stats: PartitionStats) extends ForeachWriter[String] {
+class ForeachWriterImpl(statsMap: Map[PartitionStats.Value, LongAccumulator]) extends ForeachWriter[String] {
   private final val LOGGER = LoggerFactory.getLogger(this.getClass)
 
   override def open(partitionId: Long, epochId: Long): Boolean = {
     LOGGER.info(s"Open partition $partitionId, epoch $epochId")
-    stats.incMetric(PartitionStats.partitionsOpened, 1)
+    PartitionStats.incMetric(statsMap, PartitionStats.partitionsOpened, 1)
     true
   }
 
   override def process(value: String): Unit = {
     LOGGER.info(s"Process value: $value")
-    stats.incMetric(PartitionStats.partitionsProcessed, 1)
+    PartitionStats.incMetric(statsMap, PartitionStats.partitionsProcessed, 1)
   }
 
   override def close(errorOrNull: Throwable): Unit = {
     LOGGER.info(s"Close partition: $errorOrNull")
-    stats.incMetric(PartitionStats.partitionsClosed, 1)
+    PartitionStats.incMetric(statsMap, PartitionStats.partitionsClosed, 1)
   }
 }
 
 object PartitionStats extends Enumeration {
+  private final val LOGGER = LoggerFactory.getLogger(this.getClass)
+
   final val partitionsOpened = Value("partitionsOpened")
   final val partitionsProcessed = Value("partitionsProcessed")
   final val partitionsClosed = Value("partitionsClosed")
-}
 
-class PartitionStats(@transient sparkContext: SparkContext) extends Serializable {
-  private final val LOGGER = LoggerFactory.getLogger(this.getClass)
-
-  val statsMap: Map[String, LongAccumulator] =
-    PartitionStats.values.unsorted.map(elem => elem.toString -> sparkContext.longAccumulator(elem.toString)).toMap
-
-  def incMetric(stat: PartitionStats.Value, count: Long): Unit = {
-    statsMap.get(stat.toString) match {
+  def incMetric(statsMap: Map[PartitionStats.Value, LongAccumulator], stat: PartitionStats.Value, count: Long): Unit = {
+    statsMap.get(stat) match {
       case Some(acc) => acc.add(count)
       case _ => LOGGER.error(s"Cannot increment accumulator for $stat")
     }
+  }
+}
+
+class PartitionStats(sparkContext: SparkContext) extends MetricSet {
+  private final val LOGGER = LoggerFactory.getLogger(this.getClass)
+
+  private final val statsMap: Map[PartitionStats.Value, LongAccumulator] =
+    PartitionStats.values.unsorted.map(elem => elem -> sparkContext.longAccumulator(elem.toString)).toMap
+
+  def getStatsMap: Map[PartitionStats.Value, LongAccumulator] = statsMap
+
+  override def getMetrics: util.Map[String, Metric] = {
+    val metricsMap: Map[String, Metric] = statsMap.map(
+      e =>
+        (
+          e._1.toString,
+          new Gauge[Long]() {
+            override def getValue: Long = {
+              val metricValue = e._2.value
+              e._2.reset()
+              if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(s"${e._1}: $metricValue")
+              }
+              metricValue
+            }
+          }
+        )
+    )
+
+    import scala.jdk.CollectionConverters._
+    metricsMap.asJava
+  }
+}
+
+package org.apache.spark.metrics.source {
+  case class StatsSource(srcName: String, registry: MetricRegistry) extends Source {
+    override def sourceName: String = srcName
+
+    override def metricRegistry: MetricRegistry = registry
   }
 }
